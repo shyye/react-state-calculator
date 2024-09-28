a7a7997edd7db1314b1f07a432ea8d105640a07e
shyye <emma.art.dev@gmail.com>
2024-09-28 15:51:13 +0200

    Fix Core

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,74 @@
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+  const [operator, setOperator] = useState("+");
+  const [sum, setSum] = useState(0);
+
+  function handleNumber(e) {
+    // Ignore clicks on div container
+    if (e.target.getAttribute("class") === "numbers") {
+      return;
+    }
+
+    // Reset number on Clear
+    const number = e.target.textContent === "Clear" ? 0 : e.target.textContent;
+
+    // Check if first or second number should be updated
+    const parentClass = e.currentTarget.getAttribute("id");
+    parentClass === "first-number"
+      ? setFirstNumber(number)
+      : setSecondNumber(number);
+
+    if (parentClass === "first-number") {
+      setFirstNumber(number);
+    } else if (parentClass === "second-number") {
+      setSecondNumber(number);
+    }
+  }
+
+  function handleOperator(e) {
+    // Ignore clicks on div container
+    if (e.target.getAttribute("class") === "numbers") {
+      return;
+    }
+    setOperator(e.target.textContent);
+  }
+
+  function handleCalculation() {
+    const first = parseInt(firstNumber);
+    const second = parseInt(secondNumber);
+
+    switch (operator) {
+      case "+":
+        setSum(first + second);
+        break;
+
+      case "-":
+        setSum(first - second);
+        break;
+
+      case "*":
+        setSum(first * second);
+        break;
+
+      case "÷":
+        setSum(first / second);
+        break;
+
+      default:
+        setSum(-1);
+        break;
+    }
+  }
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
-        <div className="numbers">
+        <p>{firstNumber}</p>
+        <div className="numbers" id="first-number" onClick={handleNumber}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -22,8 +84,8 @@ function App() {
       </div>
 
       <div className="panel">
-        <p>+</p>
-        <div className="numbers">
+        <p>{operator}</p>
+        <div className="numbers" onClick={handleOperator}>
           <button>+</button>
           <button>-</button>
           <button>*</button>
@@ -32,8 +94,8 @@ function App() {
       </div>
 
       <div className="panel">
-        <p>0</p>
-        <div className="numbers">
+        <p>{secondNumber}</p>
+        <div className="numbers" id="second-number" onClick={handleNumber}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -48,13 +110,13 @@ function App() {
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{sum}</p>
         <div>
-          <button>=</button>
+          <button onClick={handleCalculation}>=</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
